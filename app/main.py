35ee3c1c111bb8c78197f9e4665c32df24f5915f
atalamahardika <atalamahardika24@gmail.com>
2025-06-12 01:55:24 +0700

    Update CORS to url sibi-frontend

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ logger.info("🚀 Aplikasi FastAPI berhasil dimulai.")
 # CORS (izinkan akses dari Laravel)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000", "http://127.0.0.1:8000"],
+    allow_origins=["https://sibi-frontend-production.up.railway.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
